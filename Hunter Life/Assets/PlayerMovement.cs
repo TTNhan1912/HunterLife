9585de0c13cfe8d0b617da210cb08cff1d7020a2
TTNhan1912 <100210840+TTNhan1912@users.noreply.github.com>
2023-09-27 16:33:16 +0700

    new blend tree

---
+++ Hunter Life/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    public Rigidbody2D rigidbody2D;
+    public Animator ani;
+    Vector2 movement;   
+
+
+
+    // Update is called once per frame
+    void Update() // theo frame
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        ani.SetFloat("Horizontal", movement.x);
+        ani.SetFloat("Vertical", movement.y);
+        ani.SetFloat("Speed", movement.sqrMagnitude );
+
+    }
+
+    private void FixedUpdate() // 50 lần mỗi giây
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + movement * moveSpeed * Time.deltaTime);
+    }
+
+
+}
