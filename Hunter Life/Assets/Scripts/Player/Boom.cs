3330dfcd2b4de237597ce9085afde856e67a992c
TTNhan1912 <100210840+TTNhan1912@users.noreply.github.com>
2023-10-03 10:09:50 +0700

    datboom

--- Hunter Life/Assets/Scripts/Player/Boom.cs
+++ Hunter Life/Assets/Scripts/Player/Boom.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Boom : MonoBehaviour
 {
+    private Animator ani;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Invoke("BoomNo", 1f);
     }
+
+
+    private void BoomNo()
+    {
+        ani.SetBool("IsBoomNo", true);
+        Destroy(gameObject, 0.5f);
+    }
+
+
 }
