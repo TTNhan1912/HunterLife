269404e78c0cec71b7e0e20de5c7e1440387b8d8
thasosod <126317414+MoghTheOmen@users.noreply.github.com>
2023-10-29 11:12:25 +0700

    Teleport
    working

---
+++ Hunter Life/Assets/Scripts/Thang/PlayerTeleport.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleport : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("teleporter"))
+        {
+            currentTeleporter = null;
+        }
+    }
+}
