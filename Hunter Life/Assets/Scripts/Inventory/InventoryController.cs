461f1143717b6e4f3806747f19fcad7080f618d8
TTNhan1912 <100210840+TTNhan1912@users.noreply.github.com>
2023-11-16 22:17:02 +0700

    Update InventoryController.cs

--- Hunter Life/Assets/Scripts/Inventory/InventoryController.cs
+++ Hunter Life/Assets/Scripts/Inventory/InventoryController.cs
@@ -1,6 +1,7 @@
 using Inventory.Model;
 using Inventory.UI;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Inventory
@@ -13,12 +14,35 @@ namespace Inventory
         [SerializeField]
         private InventorySO inventoryData;
 
+        public List<InventoryItem> initalItems = new List<InventoryItem>();
 
+        private Login login;
 
         void Start()
         {
             PrepareUI();
-            //  inventoryData.Initialize();
+            PrepareInventoryData();
+        }
+
+        private void PrepareInventoryData()
+        {
+            inventoryData.Initialize();
+            inventoryData.OnInventoryUpdated += UpdateInventoryUI;
+            foreach (InventoryItem item in initalItems)
+            {
+                if (item.IsEmty)
+                    continue;
+                inventoryData.AddItem(item);
+            }
+        }
+
+        private void UpdateInventoryUI(Dictionary<int, InventoryItem> inventoryState)
+        {
+            inventoryUI.ResetAllItem();
+            foreach (var item in inventoryState)
+            {
+                inventoryUI.UpdateData(item.Key, item.Value.itemSO.IteamImage, item.Value.quantity);
+            }
         }
 
         private void PrepareUI()
@@ -40,12 +64,20 @@ namespace Inventory
 
         private void HandleDragging(int itemindex)
         {
-            throw new NotImplementedException();
+            InventoryItem inventoryItem = inventoryData.GetItemAt(itemindex);
+
+            if (inventoryItem.IsEmty)
+                return;
+
+
         }
 
         private void HandleSwapItem(int itemIndex_1, int itemIndex_2)
         {
-            throw new NotImplementedException();
+            inventoryData.SwapItems(itemIndex_1, itemIndex_2);
+
+
+
         }
 
         private void HandleDesciptionRequets(int itemindex)
@@ -59,6 +91,8 @@ namespace Inventory
             }
 
             ItemSO item = inventoryItem.itemSO;
+            //  TestModel model;
+
             inventoryUI.UpdateDesciption(itemindex, item.IteamImage, item.name, item.Description);
 
 
@@ -67,13 +101,14 @@ namespace Inventory
 
         }
 
-        void Update()
+        public void Update()
         {
             if (Input.GetKeyDown(KeyCode.Tab))
             {
                 if (inventoryUI.isActiveAndEnabled == false)
                 {
                     inventoryUI.Show();
+                    //  login.test();
                     foreach (var item in inventoryData.GetCurrentInventoryState())
                     {
                         inventoryUI.UpdateData(item.Key,
