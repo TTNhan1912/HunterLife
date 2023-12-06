1300071c57c4021fd2aee8895401d7c10ec94437
TTNhan1912 <100210840+TTNhan1912@users.noreply.github.com>
2023-12-06 14:35:29 +0700

    update

---
+++ Hunter Life/Assets/Scripts/Event.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Event : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void LogoutGame()
+    {
+        Application.Quit();
+    }
+}
