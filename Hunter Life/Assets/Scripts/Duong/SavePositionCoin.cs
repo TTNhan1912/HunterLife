b71ff1cf95e69d7d77ab78e83cee38a56e4320d4
KhanhDuonggg <117138446+KhanhDuonggg@users.noreply.github.com>
2023-12-07 10:38:57 +0700

    kd

--- Hunter Life/Assets/Scripts/Duong/SavePositionCoin.cs
+++ Hunter Life/Assets/Scripts/Duong/SavePositionCoin.cs
@@ -13,7 +13,16 @@ public class SavePositionCoin : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        if (Login.loginResponse != null)
+        {
+            if (Login.loginResponse.positionX != "")
+            {
+                float x = float.Parse(Login.loginResponse.positionX);
+                float y = float.Parse(Login.loginResponse.positionY);
+                
+                transform.position = new Vector3(x, y, 0);
+            }
+        }
     }
 
     // Update is called once per frame
